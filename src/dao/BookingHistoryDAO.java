a2eb7b6110582ea599b619f518183bab7c807702
bharatkc10 <bharatkc1011@gmail.com>
2025-12-30 08:10:51 +0545

    search bar searchable

--- src/dao/BookingHistoryDAO.java
+++ src/dao/BookingHistoryDAO.java
@@ -29,6 +29,7 @@ public class BookingHistoryDAO {
         try {
             Connection con = DBConnection.getConnection();
             PreparedStatement ps = con.prepareStatement(sql);
+            
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
@@ -55,4 +56,43 @@ public class BookingHistoryDAO {
 
         return list;
     }
+    
+    
+    
+    public List<VehicleBooking> searchByCustomerName(String name) {
+
+    List<VehicleBooking> list = new ArrayList<>();
+
+    String sql = """
+        SELECT booking_id, full_name, pickup_date, return_date,
+               pickup_location, return_location, driver_option
+        FROM vehicle_booking
+        WHERE full_name LIKE ?
+    """;
+
+    try (Connection con = DBConnection.getConnection();
+         PreparedStatement pst = con.prepareStatement(sql)) {
+
+        pst.setString(1, "%" + name + "%");
+        ResultSet rs = pst.executeQuery();
+
+        while (rs.next()) {
+            VehicleBooking v = new VehicleBooking();
+            v.bookingId = rs.getInt("booking_id");
+            v.fullName = rs.getString("full_name");
+            v.pickupDate = rs.getDate("pickup_date");
+            v.returnDate = rs.getDate("return_date");
+            v.pickupLocation = rs.getString("pickup_location");
+            v.returnLocation = rs.getString("return_location");
+            v.driverOption = rs.getString("driver_option");
+
+            list.add(v);
+        }
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return list;
+}
+
 }
